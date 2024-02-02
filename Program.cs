37f25c43f13d8e1626252e89224e1bb0b286cdec
prajwal <prajwalthete98@gmail.com>
2024-02-02 16:05:06 +0530

    first commit of UC-2

--- Program.cs
+++ Program.cs
@@ -1,28 +1,38 @@
 ﻿namespace a
 {
-    
-    // UC1- Check If Employee is Present Or Not 
+
+    // UC2- calculate Daily Employee Wage
+
     class Program
     {
-        // Constants
-        const int IS_FULL_TIME = 1;
-
         static void Main(string[] args)
         {
+            // Constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
 
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
+
+         
             int empCheck = random.Next(0, 2);
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
+
 
 
 }
