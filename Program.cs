14a3cadb5c343f6020dcdadc0644cdf3ba72e4d9
prajwal <prajwalthete98@gmail.com>
2024-02-02 15:30:02 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Computation Progarm On Master Branch ");
+        }
+    }
+}
