4571193e556c80e0a5038f945d41c72ece2616ba
prajwal <prajwalthete98@gmail.com>
2024-02-02 16:51:03 +0530

    first commit in UC-3

--- Program.cs
+++ Program.cs
@@ -1,26 +1,41 @@
 ﻿namespace a
 {
-    
-    // UC1- Check If Employee is Present Or Not 
+
+    // UC3- Add Part time Employee & Wage
+
+
     class Program
     {
-        // Constants
-        const int IS_FULL_TIME = 1;
-
         static void Main(string[] args)
         {
+            // Constants
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
 
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
 
-            if (empCheck == IS_FULL_TIME)
+            // Computation
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == IS_PART_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 
