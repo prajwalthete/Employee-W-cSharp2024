50b098e349cd41f947e1a038c52da4f788f26a77
prajwal <prajwalthete98@gmail.com>
2024-02-02 22:21:41 +0530

    second commit in UC-4

--- Program.cs
+++ Program.cs
@@ -1,28 +1,43 @@
 ﻿namespace a
 {
-    
-    // UC1- Check If Employee is Present Or Not 
+
+    // UC4- Solving Using Case Statement.  
+    using System;
+
     class Program
     {
-        // Constants
-        const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
 
         static void Main(string[] args)
         {
-
+            
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
 
-            if (empCheck == IS_FULL_TIME)
+
+            int empCheck = random.Next(0, 3);
+
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
+
 
 
 }
