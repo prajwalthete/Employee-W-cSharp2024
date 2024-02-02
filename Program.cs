0534d6e4331575a2eb92ff6ff4d4d6bb19b12645
prajwal <prajwalthete98@gmail.com>
2024-02-02 15:43:55 +0530

    first commit in UC-1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace a
 {
+    
+    // UC1- Check If Employee is Present Or Not 
     class Program
     {
+        // Constants
+        const int IS_FULL_TIME = 1;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employee Wage Computation Progarm On Master Branch ");
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
+
+
 }
