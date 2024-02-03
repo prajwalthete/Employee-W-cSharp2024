a2369c7068fdfa9105497c58de779bf0af7a7b0d
prajwal <prajwalthete98@gmail.com>
2024-02-03 12:20:14 +0530

    first UC-5 commit

--- Program.cs
+++ Program.cs
@@ -1,26 +1,56 @@
 ﻿namespace a
 {
-    
-    // UC1- Check If Employee is Present Or Not 
+
+    // UC5- Calculating Wages till Number of Working Days or
+    // Total Working Hours per month is Reached
     class Program
     {
-        // Constants
-        const int IS_FULL_TIME = 1;
+        // Constants representing employee types and wage rate
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
 
         static void Main(string[] args)
         {
+            // Variables to track employee hours and working days
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            // Loop until either the maximum working hours or working days are reached
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                // Increment the working day
+                totalWorkingDays++;
 
-            if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is Present");
+               
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                // Determine employee hours based on type
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                
+                totalEmpHrs += empHrs;
+
+                // Print the daily employee hours
+                Console.WriteLine($"Day#: {totalWorkingDays} Emp Hrs: {empHrs}");
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+
+            // Calculate and print the total employee wage
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine($"Total Emp Wage: {totalEmpWage}");
         }
     }
 
