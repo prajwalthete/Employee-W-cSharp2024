723d7ec56b685a4524439c6bbcb09a19a36a6302
prajwal <prajwalthete98@gmail.com>
2024-02-03 13:50:21 +0530

    first Commit in UC-9

--- Program.cs
+++ Program.cs
@@ -1,28 +1,80 @@
-﻿namespace a
+﻿using System;
+
+class Program
 {
-    
-    // UC1- Check If Employee is Present Or Not 
-    class Program
+    static void Main(string[] args)
     {
-        // Constants
-        const int IS_FULL_TIME = 1;
+        // Creating instances for companies using EmphageBuilderObject
+        EmphageBuilderObject dart = new EmphageBuilderObject("DMart", 20, 2, 10);
+        EmphageBuilderObject reliance = new EmphageBuilderObject("Reliance", 10, 4, 28);
 
-        static void Main(string[] args)
-        {
+        // Computing and displaying employee wages
+        dart.ComputeEmpWage();
+        Console.WriteLine(dart.ToString());
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
+        reliance.ComputeEmpWage();
+        Console.WriteLine(reliance.ToString());
+    }
+}
 
-            if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is Present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
-        }
+public class EmphageBuilderObject
+{
+    // Constants for employee types
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+
+    // Private fields
+    private string company;
+    private int empRatePerHour;
+    private int numOfWorkingDays;
+    private int maxHoursPerMonth;
+    private int totalEmpWage;
+
+    // Constructor
+    public EmphageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+    {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
     }
 
+    // Method to compute employee wage
+    public void ComputeEmpWage()
+    {
+        // Variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
+        // Computation
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+        {
+            totalWorkingDays++;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+
+            totalEmpHrs += empHrs;
+            Console.WriteLine($"Day#: {totalWorkingDays} Emp Hrs: {empHrs}");
+        }
+
+        totalEmpWage = totalEmpHrs * empRatePerHour;
+    }
+
+    // Override ToString method for custom display
+    public override string ToString()
+    {
+        return $"Total Emp Wage for company {company} is: {totalEmpWage}";
+    }
 }
--- obj/Debug/net8.0/BridgeTask1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BridgeTask1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BridgeTask1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a3cadb5c343f6020dcdadc0644cdf3ba72e4d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0534d6e4331575a2eb92ff6ff4d4d6bb19b12645")]
 [assembly: System.Reflection.AssemblyProductAttribute("BridgeTask1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BridgeTask1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
