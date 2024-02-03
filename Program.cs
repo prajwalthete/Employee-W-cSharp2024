732927fdef455b00cc96797d2ad63ae1aff8d3fb
prajwal <prajwalthete98@gmail.com>
2024-02-03 13:36:18 +0530

    first Commit in UC-8

--- Program.cs
+++ Program.cs
@@ -1,28 +1,51 @@
-﻿namespace a
+﻿using System;
+using System.Net.NetworkInformation;
+namespace a;
+
+
+// UC-8 Compute Employee Wage for Multiple Company in Procedural Way Using Class Methods
+class Program
 {
-    
-    // UC1- Check If Employee is Present Or Not 
-    class Program
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+
+    public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
     {
-        // Constants
-        const int IS_FULL_TIME = 1;
+        // Variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-        static void Main(string[] args)
+        // Computation
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
         {
-
+            totalWorkingDays++;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+
+            totalEmpHrs += empHrs;
+            Console.WriteLine($"Day#:{totalWorkingDays} Emp Hrs: {empHrs}");
         }
+
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
+        Console.WriteLine($"Total Emp Wage for company {company} is: {totalEmpWage}");
+        return totalEmpWage;
     }
 
-
+    static void Main(string[] args)
+    {
+        ComputeEmpWage("DMart", 20, 2, 10);
+        ComputeEmpWage("Reliance", 10, 4, 20);
+    }
 }
