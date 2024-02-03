7508ea147b3a51856057432274944797a759a3c2
prajwal <prajwalthete98@gmail.com>
2024-02-03 13:28:13 +0530

    first Commit in UC-7

--- Program.cs
+++ Program.cs
@@ -1,28 +1,56 @@
-﻿namespace a
+﻿
+using System.Security.Claims;
+
+namespace a;
+
+//UC-7 Compute Employee Wage Using Class Methods
+
+class Program
 {
-    
-    // UC1- Check If Employee is Present Or Not 
-    class Program
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+    public const int NUM_OF_WORKING_DAYS = 2;
+    public const int MAX_HRS_IN_MONTH = 10;
+
+    // Method to compute employee wage
+    public static int ComputeEmpWage()
     {
-        // Constants
-        const int IS_FULL_TIME = 1;
+        // Variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-        static void Main(string[] args)
+        // Computation
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
-
+            totalWorkingDays++;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+
+            totalEmpHrs += empHrs;
+            Console.WriteLine($"Day#: {totalWorkingDays} Emp Hrs: {empHrs}");
         }
+
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine($"Total Emp Wage : {totalEmpWage}");
+        return totalEmpWage;
     }
 
+    static void Main(string[] args)
+    {
 
+        ComputeEmpWage();
+    }
 }
